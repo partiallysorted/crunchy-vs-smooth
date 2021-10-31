6f59baff582e602a308087665745dfb232795e2d
Partially Sorted <mas@jetprotocol.io>
2021-10-31 11:04:58 -0500

    #

--- README.md
+++ README.md
@@ -5,3 +5,133 @@ Tutorial from https://www.brianfriel.xyz/learning-how-to-build-on-solana/
 anchor init crunchy-vs-smooth
 ```
 
+Get the ProgramId
+
+```
+solana address -k target/deploy/crunchy_vs_smooth-keypair.json
+```
+
+Paste into programs/crunchy-vs-smooth/src/lib.rs and Anchor.toml
+
+Check that Solana is using localhost
+
+```
+solana config get
+```
+
+If not,
+
+```
+solana config set --url localhost
+```
+
+Test that Solana is ready
+
+```
+solana-test-validator
+```
+
+Check address and balance.
+
+```
+solana address
+solana balance
+```
+
+Airdrop if necessary.
+
+```
+solana airdrop 1000
+```
+
+Kill the solana-test-validator.
+
+Run anchor test
+
+```
+anchor test
+```
+
+To deploy the program
+```
+solana-test-validator
+
+solana logs
+
+anchor deploy
+```
+
+To create React app
+```
+npx create-react-app app
+
+cd app
+​
+npm install @project-serum/anchor \
+@solana/web3.js \
+@solana/wallet-adapter-react \
+@solana/wallet-adapter-wallets \
+@solana/wallet-adapter-base
+
+npm install @material-ui/core \
+@material-ui/icons \
+@solana/wallet-adapter-material-ui \
+notistack
+```
+
+Anchor does not automatically copy the IDL to the app directory.
+```
+cp ../target/idl/crunchy_vs_smooth.json src/idl.json
+
+npm start
+```
+
+Connect Phantom wallet and fund it.
+
+```
+solana airdrop 1000 <your-phantom-address-here>
+```
+
+Install express as a proxy.
+```
+cd ..
+
+npm install express
+```
+
+Add index.js to the root directory
+```
+const path = require("path");
+const anchor = require("@project-serum/anchor");
+const express = require("express");
+
+const voteAccount = anchor.web3.Keypair.generate();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "app/build")));
+
+app.get("/voteAccount", (req, res) => {
+  res.json({ voteAccount });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "app/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+```
+
+Run express
+```
+node index.js
+```
+
+Restart the React app
+```
+npm start
+```
--- tests/crunchy-vs-smooth.js
+++ tests/crunchy-vs-smooth.js
@@ -1,14 +1,74 @@
+const assert = require("assert");
 const anchor = require('@project-serum/anchor');
+const { SystemProgram } = anchor.web3;
 
 describe('crunchy-vs-smooth', () => {
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.CrunchyVsSmooth;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  const program = anchor.workspace.CrunchyVsSmooth;
+
+  const voteAccount = anchor.web3.Keypair.generate();
+
+  it("Initializes with 0 votes for crunchy and smooth", async () => {
+    console.log("Testing Initialize...");
+
+    await program.rpc.initialize({
+      accounts : {
+        voteAccount: voteAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [voteAccount],
+    });
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(account.crunchy.toString() == 0);
+    assert.ok(account.smooth.toString() == 0);
   });
+
+  it("Votes correctly for crunchy", async () => {
+    console.log("Testing voteCrunchy...");
+
+    await program.rpc.voteCrunchy({
+      accounts : {
+        voteAccount: voteAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(account.crunchy.toString() == 1);
+    assert.ok(account.smooth.toString() == 0);
+  });
+
+  it("Votes correctly for smooth", async () => {
+    console.log("Testing voteSmooth...");
+
+    await program.rpc.voteSmooth({
+      accounts : {
+        voteAccount: voteAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.voteAccount.fetch(
+      voteAccount.publicKey
+    );
+
+    console.log("Crunchy: ", account.crunchy.toString());
+    console.log("Smooth: ", account.smooth.toString());
+    assert.ok(account.crunchy.toString() == 1);
+    assert.ok(account.smooth.toString() == 1);
+  });
+  
 });
